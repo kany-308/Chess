97f28b9a302041a57a57d21be2946aa4ad1b291c
Kanhaiya Singh <kanhaiya.singh@wolterskluwer.com>
2020-07-20 18:46:32 +0530

    Implemented the Strategy of Horse's possible movements

--- src/main/java/com/kany/chess/Horse.java
+++ src/main/java/com/kany/chess/Horse.java
@@ -1,11 +1,32 @@
 package com.kany.chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Horse implements Piece {
 
   @Override
   public String possibleMovement(String position) {
-    // TODO Auto-generated method stub
-    return null;
+    int i = Helper.getRowIndexFromPosition(position);
+    int j = Helper.getColumnIndexFromPosition(position);
+    List<String> result = new ArrayList<>();
+    if (Helper.isValidIndex(i - 2, j + 1))
+      result.add(Helper.getPosition(i - 2, j + 1));
+    if (Helper.isValidIndex(i - 1, j + 2))
+      result.add(Helper.getPosition(i - 1, j + 2));
+    if (Helper.isValidIndex(i + 1, j + 2))
+      result.add(Helper.getPosition(i + 1, j + 2));
+    if (Helper.isValidIndex(i + 2, j + 1))
+      result.add(Helper.getPosition(i + 2, j + 1));
+    if (Helper.isValidIndex(i + 2, j - 1))
+      result.add(Helper.getPosition(i + 2, j - 1));
+    if (Helper.isValidIndex(i + 1, j - 2))
+      result.add(Helper.getPosition(i + 1, j - 2));
+    if (Helper.isValidIndex(i - 1, j - 2))
+      result.add(Helper.getPosition(i - 1, j - 2));
+    if (Helper.isValidIndex(i - 2, j - 1))
+      result.add(Helper.getPosition(i - 2, j - 1));
+    return String.join(",", result);
   }
 
 }
