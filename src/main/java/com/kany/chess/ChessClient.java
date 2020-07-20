437685df501944f0fe941b250f03b301515691db
Kanhaiya Singh <kanhaiya.singh@wolterskluwer.com>
2020-07-20 16:58:30 +0530

    Created the project structure

---
+++ src/main/java/com/kany/chess/ChessClient.java
@@ -0,0 +1,10 @@
+package com.kany.chess;
+
+public class ChessClient {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
---
+++ src/test/java/com/kany/chess/ChessClientTest.java
@@ -0,0 +1,13 @@
+package com.kany.chess;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class ChessClientTest {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
