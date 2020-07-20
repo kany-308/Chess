ab96c4a3ae9f63143251a20cde89c9a5844efc24
Kanhaiya Singh <kanhaiya.singh@wolterskluwer.com>
2020-07-20 18:53:22 +0530

    Implemented the Strategy of Pawn's possible movements

--- src/main/java/com/kany/chess/Pawn.java
+++ src/main/java/com/kany/chess/Pawn.java
@@ -1,10 +1,20 @@
 package com.kany.chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pawn implements Piece {
 
   @Override
+  // Assuming Pawn's movement from both the sides
   public String possibleMovement(String position) {
-    // TODO Auto-generated method stub
-    return null;
+    int i = Helper.getRowIndexFromPosition(position);
+    int j = Helper.getColumnIndexFromPosition(position);
+    List<String> result = new ArrayList<>();
+    if (Helper.isValidIndex(i - 1, j))
+      result.add(Helper.getPosition(i - 1, j));
+    if (Helper.isValidIndex(i + 1, j))
+      result.add(Helper.getPosition(i + 1, j));
+    return String.join(",", result);
   }
 }
